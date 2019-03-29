2de820158e39b8156134ee4500b8673c951a3650
Emily Morgado <emilyannemorgado@gmail.com>
2019-03-28 22:07:33 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Emily Morgado</h1>
+        <p>A brief about...</p>
+        <ul>
+          <li>articles</li>
+          <li>projects</li>
+          <li>passions</li>
+        </ul>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
